0faaf78917f515e7bac0fdd4b174061334fa2b5f
geony_kkk <blueds05@gmail.com>
2024-07-15 16:45:54 +0900

    [feature] 구조 변경된 SSD Class 기준, Write 기능 TestCase 추가

--- ssd/test_virtual_ssd.py
+++ ssd/test_virtual_ssd.py
@@ -11,21 +11,12 @@ class Test(TestCase):
     def test_virtual_ssd_write_1(self) -> None:
         # arrange
         ssd = VirtualSSD()
+        addr_will_be_write = 0
         data_will_be_write = "0xAAAABBBB"
-        block_will_be_write = Block4Byte(0, data_will_be_write)
-        nand_csv_file_path = "test_nand.csv"
-        if os.path.exists(nand_csv_file_path):
-            os.remove(nand_csv_file_path)
 
         # act
-        ssd.write(block_will_be_write)
+        ssd.write(addr_will_be_write, data_will_be_write)
 
         # assert
-        df = pd.read_csv(
-            nand_csv_file_path, delimiter=" ", header=None, names=["Address", "Data"]
-        )
-        block_list = [
-            Block4Byte(row["Address"], row["Data"]) for index, row in df.iterrows()
-        ]
-        actual = block_list[0].get_data()
-        self.assertEqual(data_will_be_write, actual)
+        actual = ssd.nand_df.iloc[addr_will_be_write]
+        self.assertEqual(data_will_be_write, actual['Data'])
