fbd2c3f3fe42c7e6d63c1d8d1196e37f4d551c42
mandu <wbslalswns@naver.com>
2024-07-15 15:48:42 +0900

    [feat] Make Shell.py Skeleton code

--- Shell.py
+++ Shell.py
@@ -10,10 +10,36 @@ class Validate:
 
 
 class Shell:
-    def write(self):
+
+    def __init__(self):
         pass
 
-    def read(self):
+    def run(self):
+
+        while True:
+            inputs = input().split(" ")
+            if inputs[0] == "ssd" and inputs[1] == "W":
+                self.write(inputs[2], inputs[3])
+
+            if inputs[0] == "ssd" and inputs[1] == "R":
+                self.read(inputs[2])
+
+            elif inputs[0] == "exit":
+                break
+
+            elif inputs[0] == "help":
+                pass
+
+            elif inputs[0] == "fullwrite":
+                pass
+
+            elif inputs[0] == "fullread":
+                pass
+
+    def write(self, address: int, data: str) -> None:
+        pass
+
+    def read(self, address: int) -> None:
         pass
 
     def exit(self):
@@ -27,3 +53,8 @@ class Shell:
 
     def fullread(self):
         pass
+
+
+if __name__ == "__main__":
+    shell = Shell()
+    shell.run()
