bcec34ae70536d93f8c98a4af4adb818c7e70d96
geony_kkk <blueds05@gmail.com>
2024-07-16 08:48:56 +0900

    [feature] ssd.read() 기능 TestCase 구현

--- ssd/virtual_ssd.py
+++ ssd/virtual_ssd.py
@@ -3,6 +3,8 @@ from ssd.storage_device_interface import StorageDeviceInterface
 import pandas as pd
 import os
 
+INIT_VALUE = "0x00000000"
+
 
 class VirtualSSD(StorageDeviceInterface):
     def __init__(self):
@@ -10,7 +12,9 @@ class VirtualSSD(StorageDeviceInterface):
         self.result_path = "ssd/result.csv"
 
         if not os.path.exists(self.nand_path):
-            self.nand_df = pd.DataFrame(columns=["Data"], data=["0x00000000"] * 100)
+            self.nand_df = pd.DataFrame(
+                columns=["Data"], data=[("%s" % INIT_VALUE)] * 100
+            )
             self.nand_df.to_csv(self.nand_path)
 
     def write(self, addr: int, data: str) -> None:
