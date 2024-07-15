598fd47fbb7e11e98d931b689f498758fa2e231a
donghee.lim <ldh5730@naver.com>
2024-07-15 16:15:52 +0900

    unittest - test exit and helo method of Shell

---
+++ test_Shell.py
@@ -0,0 +1,38 @@
+from unittest import TestCase, skip
+from unittest.mock import patch
+
+from Shell import Shell
+
+
+class TestShell(TestCase):
+    def setUp(self):
+        self.shell = Shell()
+
+    @skip
+    @patch("builtins.input", side_effect=["exit"])
+    def test_exit(self):
+        self.shell.run()
+        self.assertFalse(self.shell.is_run)
+
+    @skip
+    @patch("builtins.input", side_effect=["help", "exit"])
+    def test_help_command(self, mock_input):
+        with patch("builtins.print") as mock_print:
+            self.shell.run()
+            mock_print.assert_any_call("Available commands:")
+            mock_print.assert_any_call(
+                "  write <LBA> <value>  - Write value to the specified LBA"
+            )
+            mock_print.assert_any_call(
+                "  read <LBA>           - Read value from the specified LBA"
+            )
+            mock_print.assert_any_call(
+                "  fullwrite <value>    - Write value to all LBAs"
+            )
+            mock_print.assert_any_call(
+                "  fullread             - Read values from all LBAs"
+            )
+            mock_print.assert_any_call("  exit                 - Exit the shell")
+            mock_print.assert_any_call(
+                "  help                 - Show this help message"
+            )
