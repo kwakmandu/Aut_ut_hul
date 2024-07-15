5f6f29148ca945571efa86c86f8ab03443877f96
geony_kkk <blueds05@gmail.com>
2024-07-15 15:00:52 +0900

    [feature] block, ssd interface/구현체 아규먼트/리턴값 타입 구체화

--- Block/block4Byte.py
+++ Block/block4Byte.py
@@ -2,7 +2,7 @@ from Block.blockInterface import BlockInterface
 
 
 class Block4Byte(BlockInterface):
-    def __init__(self, address, data):
+    def __init__(self, address: int, data: str):
         super.__init__()
         self.__address = address
         self.__data = data
