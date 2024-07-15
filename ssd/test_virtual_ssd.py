393b66172cbc8a536db6ea62c47622553a4ffe3c
geony_kkk <blueds05@gmail.com>
2024-07-16 08:48:56 +0900

    [feature] ssd.read() 기능 TestCase 구현

--- ssd/test_virtual_ssd.py
+++ ssd/test_virtual_ssd.py
@@ -19,3 +19,37 @@ class Test(TestCase):
         # assert
         actual = ssd.nand_df.iloc[addr_will_be_write]
         self.assertEqual(data_will_be_write, actual["Data"])
+
+    def test_virtual_ssd_read_case_한번도안적은곳(self) -> None:
+        # arrange
+        ssd = VirtualSSD()
+        addr_will_be_read = 30
+
+        # act
+        ssd.read(addr_will_be_read)
+
+        # result.csv read
+        file_path = "ssd/result.csv"
+        with open(file_path, "r", encoding="utf-8") as file:
+            actual = file.read()
+
+        # assert
+        self.assertEqual("0x00000000", actual)
+
+    def test_virtual_ssd_read_case_적은곳(self) -> None:
+        # arrange
+        ssd = VirtualSSD()
+        addr_will_be_write_read = 35
+        data_will_be_write = "0xAAAABBBB"
+
+        # act
+        ssd.write(addr_will_be_write_read, data_will_be_write)
+        ssd.read(addr_will_be_write_read)
+
+        # result.csv read
+        file_path = "ssd/result.csv"
+        with open(file_path, "r", encoding="utf-8") as file:
+            actual = file.read()
+
+        # assert
+        self.assertEqual(data_will_be_write, actual)
