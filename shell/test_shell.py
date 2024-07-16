e98d8613cd09fb4172bf0fd62fb401b3d9ae97c8
donghee.lim <ldh5730@naver.com>
2024-07-16 10:14:50 +0900

    [fix] 인터페이스 변경에 따른 read and write test case 수정

--- shell/test_shell.py
+++ shell/test_shell.py
@@ -14,14 +14,14 @@ class TestShell(TestCase):
     @patch.object(VirtualSSD, "write")
     def test_write_command(self, mock_write, mock_input) -> None:
         self.shell.run()
-        mock_write.assert_called_with("3", "0x1298CDEF")
+        mock_write.assert_called_with(3, "0x1298CDEF")
 
     @patch("builtins.input", side_effect=["ssd R 3", "exit"])
     @patch.object(VirtualSSD, "read", return_value="0x1298CDEF")
     def test_read_command(self, mock_read, mock_input) -> None:
         with patch("builtins.print") as mock_print:
             self.shell.run()
-            mock_read.assert_called_with("3")
+            mock_read.assert_called_with(3)
             mock_print.assert_any_call("0x1298CDEF")
 
     @patch("builtins.input", side_effect=["exit"])
