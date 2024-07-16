a67d27e1a38f4e6dd2a0afa325de7700fb841e12
seungju <seungjju.kim@samsung.com>
2024-07-16 09:22:45 +0900

    [refactoring] 절대경로 수정 및 이전 파일 제거

--- ssd/virtual_ssd.py
+++ ssd/virtual_ssd.py
@@ -1,5 +1,4 @@
-import sys
-
+from block.block_4byte import Block4Byte
 from ssd.storage_device_interface import StorageDeviceInterface
 import pandas as pd
 import os
@@ -34,11 +33,3 @@ class VirtualSSD(StorageDeviceInterface):
         )
         result_df = result_df.replace("\n", "")
         result_df.to_csv(self.result_path, index_label="index")
-
-if __name__ == '__main__':
-    ssd = VirtualSSD()
-    cmd, address, value = sys.argv[1:]
-    if cmd == 'W':
-        ssd.write(int(address), value)
-    elif cmd == 'R':
-        ssd.read(int(address))
