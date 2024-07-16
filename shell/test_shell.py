30131583fe15d6415ace70d35e473ce54c156732
donghee.lim <ldh5730@naver.com>
2024-07-16 10:01:29 +0900

    [unittest] - exit and help method of Shell

--- shell/test_shell.py
+++ shell/test_shell.py
@@ -1,4 +1,4 @@
-from unittest import TestCase
+from unittest import TestCase, skip
 from unittest.mock import Mock, patch
 
 from shell import Shell
@@ -23,3 +23,32 @@ class TestShell(TestCase):
             self.shell.run()
             mock_read.assert_called_with("3")
             mock_print.assert_any_call("0x1298CDEF")
+
+    @skip
+    @patch("builtins.input", side_effect=["exit"])
+    def test_exit(self, mock_input):
+        self.shell.run()
+        self.assertFalse(self.shell.is_run)
+
+    @skip
+    @patch("builtins.input", side_effect=["help", "exit"])
+    def test_help_command(self, mock_input):
+        with patch("builtins.print") as mock_print:
+            self.shell.run()
+            mock_print.assert_any_call("Available commands:")
+            mock_print.assert_any_call(
+                "  write <LBA> <value>  - Write value to the specified LBA"
+            )
+            mock_print.assert_any_call(
+                "  read <LBA>           - Read value from the specified LBA"
+            )
+            mock_print.assert_any_call(
+                "  fullwrite <value>    - Write value to all LBAs"
+            )
+            mock_print.assert_any_call(
+                "  fullread             - Read values from all LBAs"
+            )
+            mock_print.assert_any_call("  exit                 - Exit the shell")
+            mock_print.assert_any_call(
+                "  help                 - Show this help message"
+            )
