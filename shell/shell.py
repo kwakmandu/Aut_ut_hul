668926f1b73950fedafc99d262b6fc2b3de5bbc0
seungju <seungjju.kim@samsung.com>
2024-07-17 17:01:15 +0900

    [fix] erase_range 시 int 수정

--- shell/shell.py
+++ shell/shell.py
@@ -108,7 +108,7 @@ class Shell:
             case "erase":
                 self.erase(inputs[1], inputs[2])
             case "erase_range":
-                self.erase(inputs[1], inputs[2] - inputs[1])
+                self.erase(inputs[1], str(int(inputs[2]) - int(inputs[1])))
             case "exit":
                 self.exit()
             case "help":
