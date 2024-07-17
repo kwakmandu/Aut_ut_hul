5b9ddd940de3c055d6aef48f78f638820d1679e8
gyu.byeon <isoul902@gmail.com>
2024-07-17 16:22:49 +0900

    [feat] strategy.py update와 read 알고리즘 구현

--- ssd/strategy.py
+++ ssd/strategy.py
@@ -14,10 +14,62 @@ class InterfaceStrategy(ABC):
 
 class DequeStrategy(InterfaceStrategy):
     def update(self, cmdlist, new_cmd):
-        new_cmdlist = deque(cmdlist)
+        deque_cmdlist = deque(cmdlist)
+        updated_cmdlist = list()
 
-        return new_cmdlist
+        cmd_type, address, value = new_cmd
+        if cmd_type == "W":
+            while deque_cmdlist:
+                old_cmd = deque_cmdlist.popleft()
+                old_cmd_type, old_address, old_value = old_cmd
+                if old_cmd_type == "W" and old_address == address:
+                    continue
+                updated_cmdlist = self._delete_overlap_and_add_cmd(
+                    updated_cmdlist, old_cmd
+                )
+            updated_cmdlist = self._delete_overlap_and_add_cmd(updated_cmdlist, new_cmd)
+
+        if cmd_type == "E":
+            while deque_cmdlist:
+                old_cmd = deque_cmdlist.popleft()
+                old_cmd_type, old_address, old_value = old_cmd
+                if old_cmd_type == "W" and (address <= old_address < address + value):
+                    continue
+                updated_cmdlist = self._delete_overlap_and_add_cmd(
+                    updated_cmdlist, old_cmd
+                )
+            updated_cmdlist = self._delete_overlap_and_add_cmd(updated_cmdlist, new_cmd)
+        return updated_cmdlist
+
+    def _delete_overlap_and_add_cmd(cmdlist, new_cmd):
+        if not cmdlist:
+            cmdlist.append(new_cmd)
+            return cmdlist
+
+        righttop_cmd = cmdlist[-1]
+        pop_cmd_type, pop_cmd_address, pop_cmd_value = righttop_cmd
+        new_cmd_type, new_cmd_address, new_cmd_value = new_cmd
+
+        if (pop_cmd_type == new_cmd_type == "E") and (
+            new_cmd_address <= pop_cmd_address + pop_cmd_value
+        ):
+            cmdlist.pop()
+            from_index = pop_cmd_address
+            to_index = new_cmd_address + new_cmd_value
+            cmdlist.append(["E", from_index, to_index - from_index])
+            return cmdlist
+
+        cmdlist.append(new_cmd)
+        return cmdlist
 
     def read(self, cmdlist, address):
-        value = 0  # or None
-        return value
+        if not cmdlist:
+            return None
+
+        for cmd in reversed(cmdlist):
+            cmd_type, cmd_address, cmd_value = cmd
+            if address == cmd_address:
+                if cmd_type == "W":
+                    return cmd_value
+                return "Erase"
+        return None
