8e40c8588ae41bf641421c6e997855493c16bcf1
gyu.byeon <isoul902@gmail.com>
2024-07-17 14:44:22 +0900

    [feat] buffer.csv 파일 생성 및 csvload함수 구현

--- ssd/buffer.py
+++ ssd/buffer.py
@@ -1,6 +1,10 @@
 import os
+from collections import deque
+
 import pandas as pd
 from abc import ABC, abstractmethod
+
+from ssd.strategy import DequeStrategy
 
 
 class InterfaceBuffer(ABC):
@@ -20,23 +24,43 @@ class InterfaceBuffer(ABC):
     def add_cmd(self):
         pass
 
+    @abstractmethod
+    def _load_csvfile_and_set_cmdlist(self):
+        pass
+
+    @abstractmethod
+    def select_strategy(self, strategy):
+        pass
+
 
 class Buffer(InterfaceBuffer):
     def __init__(
         self, size=10, strategy="Deque", csv_path=os.path.join(".", "buffer.csv")
     ):
-        self.super().init()
         self.size = size
-        self.strategy = strategy
-        self.cmdlist = pd.read_csv("cmdlist.csv")
+        self.strategy = self.select_strategy(strategy)
+        self.csv_path = csv_path
+        self.cmdlist = self._load_csvfile_and_set_cmdlist()
 
-        pass
+    def _load_csvfile_and_set_cmdlist(self):
+        df = pd.read_csv(self.csv_path)
+        rows_as_lists = df.values.tolist()
+        cmdlist = deque(rows_as_lists)
+        return cmdlist
 
     def get_size(self):
-        pass
+        return len(self.cmdlist)
 
     def get_cmdlist(self):
-        pass
+        return self.cmdlist
 
-    def add_cmd(self):
-        pass
+    def add_cmd(self, command, address, value):
+        new_command = (command, address, value)
+        self.strategy.optimise(self.cmdlist, new_command)
+
+    def select_strategy(self, strategy):
+        if strategy == "Deque":
+            return DequeStrategy()
+
+
+bf = Buffer()
