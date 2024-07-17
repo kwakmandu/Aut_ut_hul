a20285d0dc0c2ecf3f87843f9fef90cf1637f4b4
gyu.byeon <isoul902@gmail.com>
2024-07-17 15:14:29 +0900

    [read] buffer.py 변수, 함수명 수정 및 가독성 개선. strategy.py read 메소드 추가

--- ssd/buffer.py
+++ ssd/buffer.py
@@ -24,47 +24,52 @@ class InterfaceBuffer(ABC):
     def add_cmd(self):
         pass
 
-    @abstractmethod
-    def _load_csvfile_and_set_cmdlist(self):
-        pass
-
-    @abstractmethod
-    def select_strategy(self, strategy):
-        pass
-
 
 class Buffer(InterfaceBuffer):
     def __init__(
-        self, size=10, strategy="Deque", csv_path=os.path.join(".", "buffer.csv")
+        self,
+        cmdlist_limitsize=10,
+        strategy="Deque",
+        csv_path=os.path.join(".", "buffer.csv"),
     ):
-        self.size = size
-        self.strategy = self.select_strategy(strategy)
+        self.cmdlist_limitsize = cmdlist_limitsize
+        self.strategy = self._select_strategy(strategy)
         self.csv_path = csv_path
         self.cmdlist = self._load_csvfile_and_set_cmdlist()
+
+    def add_cmd(self, cmd_type, address, value):
+        if self._is_invalid():
+            raise Exception("Invalid error occur")
+
+        new_cmd = (cmd_type, address, value)
+        self.strategy.update(self.cmdlist, new_cmd)
+
+    def read_addressvalue_in_cmdlist(self, address):
+        rst_value = None  # value or None
+        return rst_value
+
+    def _select_strategy(self, strategy):
+        if strategy == "Deque":
+            return DequeStrategy()
+
+    def _is_invalid(self):
+        if len(self.cmdlist) >= self.cmdlist_limitsize:
+            return True
 
     def _load_csvfile_and_set_cmdlist(self):
         df = pd.read_csv(self.csv_path)
         rows_as_lists = df.values.tolist()
-        cmdlist = deque(rows_as_lists)
+        cmdlist = list(rows_as_lists)
         return cmdlist
+
+    def get_cmdlist_limitsize(self):
+        return self.cmdlist_limitsize
 
     def get_size(self):
         return len(self.cmdlist)
 
     def get_cmdlist(self):
         return self.cmdlist
-
-    def add_cmd(self, cmd_type, address, value):
-        new_cmd = (cmd_type, address, value)
-        self.strategy.update(self.cmdlist, new_cmd)
-
-    def select_strategy(self, strategy):
-        if strategy == "Deque":
-            return DequeStrategy()
-
-    def read_addressvalue_in_cmdlist(self, address):
-        rst_value = None  # value or None
-        return rst_value
 
 
 bf = Buffer()
--- ssd/strategy.py
+++ ssd/strategy.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from collections import deque
 
 
 class InterfaceStrategy(ABC):
@@ -6,7 +7,17 @@ class InterfaceStrategy(ABC):
     def update(self):
         pass
 
+    @abstractmethod
+    def read(self):
+        pass
+
 
 class DequeStrategy(InterfaceStrategy):
-    def update(self):
-        pass
+    def update(self, cmdlist, new_cmd):
+        new_cmdlist = deque(cmdlist)
+
+        return new_cmdlist
+
+    def read(self, cmdlist, address):
+        value = 0  # or None
+        return value
