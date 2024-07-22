5812f2002fc6774ad1215fed3e06287b965ec0e2
Wontae Jeong <dev.jeong@samsung.com>
2024-07-22 09:51:18 +0900

    [refactor] remove factoryinterface
    Signed-off-by: Wontae Jeong <dev.jeong@samsung.com>

--- ssd/strategy_factory.py
+++ ssd/strategy_factory.py
@@ -2,19 +2,13 @@ from abc import ABC, abstractmethod
 from ssd.strategy import DequeStrategy, InterfaceStrategy, HeapStrategy
 
 
-class InterfaceStrategyFactory(ABC):
-    @staticmethod
-    @abstractmethod
-    def select_strategy(strategy_name: str) -> InterfaceStrategy:
-        pass
-
-
-class StrategyFactory(InterfaceStrategyFactory):
+class StrategyFactory:
     @staticmethod
     def select_strategy(strategy_name: str) -> InterfaceStrategy:
-        if strategy_name.upper() == "DEQUE":
-            return DequeStrategy()
-        elif strategy_name.upper() == "HEAP":
-            return HeapStrategy()
-
-        return HeapStrategy()
+        match strategy_name.upper():
+            case "DEQUE":
+                return DequeStrategy()
+            case "HEAP":
+                return HeapStrategy()
+            case _:
+                return HeapStrategy()
